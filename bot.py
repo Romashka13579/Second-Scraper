3a1768f8986d955c7c4d03d77e0e432516cc73c0
Romashka <rroma13579@gmail.com>
2024-08-01 23:37:37 +0200

    facebook scraper

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ import time
 import json
 import os.path
 import telegram
-# import asyncio
-# bot = telegram.Bot(token='7216096099:AAFN-2lH6-IAjVhdhDk5MXJy75p8KFsXiKg')
+import asyncio
+bot = telegram.Bot(token='7216096099:AAFN-2lH6-IAjVhdhDk5MXJy75p8KFsXiKg')
 
-# async def main(item_id, name, price, description, link):
-#     async with bot:
-#         await bot.send_message(text=f'ID: {item_id}\n\nName: {name}\n\nPrice: {price}\n\nDescription: {description}\n\nLink: {link}', chat_id='984091159')
+async def main(item_id, name, price, description, link):
+    async with bot:
+        await bot.send_message(text=f'ID: {item_id}\n\nName: {name}\n\nPrice: {price}\n\nDescription: {description}\n\nLink: {link}', chat_id='984091159')
 
 
 def item_scraping(item_id, link):
@@ -19,14 +19,11 @@ def item_scraping(item_id, link):
     clickButton("x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1iyjqo2.xs83m0k.x150jy0e.x1e558r4.xjkvuk6.x1iorvi4.xdl72j9", driverForLinks)
     time.sleep(1)
     clickButton("x92rtbv.x10l6tqk.x1tk7jg1.x1vjfegm", driverForLinks)
-    time.sleep(5)
+    time.sleep(5)   
     name = driverForLinks.find_elements(By.CLASS_NAME,  "x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x14z4hjw.x3x7a5m.xngnso2.x1qb5hxa.x1xlr1w8.xzsf02u")
     price = driverForLinks.find_elements(By.CLASS_NAME,  "x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x676frb.x1lkfr7t.x1lbecb7.xk50ysn.xzsf02u")
-    description = driverForLinks.find_elements(By.CLASS_NAME,  "x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u")
-    # name = driverForLinks.find_elements(By.CLASS_NAME, "x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x14z4hjw.x3x7a5m.xngnso2.x1qb5hxa.x1xlr1w8.xzsf02u")
-    # price = driverForLinks.find_element(By.CLASS_NAME, "x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1sur9pj.xkrqix3.x1lku1pv.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x676frb.x1lkfr7t.x1lbecb7.xk50ysn.xzsf02u")
-    # description = driverForLinks.find_element(By.CLASS_NAME, "x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1sur9pj.xkrqix3.x1lku1pv.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x676frb.x1lkfr7t.x1lbecb7.xk50ysn.xzsf02u.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u")
-    
+    description = driverForLinks.find_element(By.CLASS_NAME,  "x1n2onr6.x1ja2u2z.x9f619.x78zum5.xdt5ytf.x2lah0s.x193iq5w.xx6bls6.x1jx94hy")
+
     # name = WebDriverWait(driverForLinks, 5).until(
     #     EC.element_to_be_clickable((By.CLASS_NAME, "x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1sur9pj.xkrqix3.x1lku1pv.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x676frb.x1lkfr7t.x1lbecb7.xk50ysn.xzsf02u.x14z4hjw.x3x7a5m.xngnso2.x1qb5hxa.x1xlr1w8.xzsf02u"))
     # )
@@ -38,14 +35,14 @@ def item_scraping(item_id, link):
     # )
     
     # print(name.text, price.text)
-    print(name)
-    for item in price:
-        print(item.text)
-    for item in description:
-        print(item.text)
-    # data[str(item_id)] = {"name" : name[1].text, "price": price.text, "description": description.text, "link": link}
-    # writeToJSONFile('./','savedData', data)
-    # asyncio.run(main(item_id, name.text, price.text, description.text, link))
+    print(name[1].text)
+    print("----")
+    print(price[1].text)
+    print("----")
+    print(description.text)
+    data[str(item_id)] = {"name" : name[1].text, "price": price[1].text, "description": description.text, "link": link}
+    writeToJSONFile('./','savedData', data)
+    asyncio.run(main(item_id, name[1].text, price[1].text, description.text, link))
     print("__________%s____________"% i)
 
 
@@ -101,6 +98,7 @@ while i!=0:
 
     with driver:
         driver.get("https://www.facebook.com/marketplace/category/computers")
+        clickButton("disc-wrapper", driver)
     time.sleep(2)
 
     while pages!=1 and driver.find_elements(By.CSS_SELECTOR, "[role='link']"):
